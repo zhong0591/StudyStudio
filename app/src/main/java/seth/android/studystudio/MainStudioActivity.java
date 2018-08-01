054092d4884aecf6e0d23975880e27177a895bfb
mousejoy@sohu.com <mousejoy@sohu.com>
2018-08-01 14:12:22 +0800

    Created project

---
+++ app/src/main/java/seth/android/studystudio/MainStudioActivity.java
@@ -0,0 +1,13 @@
+package seth.android.studystudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainStudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_studio);
+    }
+}
